e91e25e5a03bf31010f2783cc4e955f1d047e84e
Raquel <raquelus8@gmail.com>
2023-11-20 18:32:45 +0100

    loading in team component

--- src/app/pages/team/team.component.ts
+++ src/app/pages/team/team.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-team',
   templateUrl: './team.component.html',
   styleUrls: ['./team.component.scss']
 })
-export class TeamComponent {
-  title = 'flip-card';
+export class TeamComponent implements OnInit {
+  loading: boolean = true;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loading = false;  
+    }, 2000);  
+  }
 }
